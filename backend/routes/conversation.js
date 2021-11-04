6fc16f390d23682aa5b2fb4f2e43deb5f80cf517
Winston Leung <wleung85@uwo.ca>
2021-11-03 22:58:24 -0400

    Changed conversation to expect users to be sorted by object ID

--- backend/routes/conversation.js
+++ backend/routes/conversation.js
@@ -6,49 +6,76 @@ const router = require('express').Router();
 // TODO: user authentication
 router.post('/create', async (req, res) => {
   try {
-    if (!("usersTba" in req.body)) {
+    if (!("usersTba" in req.body) || req.body.usersTba.length === 0) {
       return res.status(500).json({message: "No passed in users to add."});
-    } else {
-      req.body.usersTba = [...new Set(req.body.usersTba)];
-      const currUserIdx = req.body.usersTba.indexOf(req.body.userId);
-      if (currUserIdx > -1) {
-        req.body.usersTba.splice(currUserIdx, 1);
-      }
+    } 
+
+    // Remove duplicates and add calling user to list. Sort list so users in 
+    // Conversation model are always listed in alphabetical order
+    req.body.usersTba = [...new Set([req.body.userId, ...req.body.usersTba])].sort();
+    
+    if (req.body.usersTba.length < 2 || req.body.usersTba.length > 50) {
+      return res.status(500).json({message: "Group conversations can have at maximum 50 members"});
     }
     
-    if (req.body.usersTba.length < 2) {
-      return res.status(500).json({message: "Group conversation can only be created with at least 3 members"});
-    } else if (req.body.usersTba.length > 49) {
-      return res.status(500).json({message: "Group conversations can have at maximum 50 members"});
-    } else {
-      const existingConvos = await Conversation.find({users: {$all: [req.body.userId, ...req.body.usersTba]}})
-      if (existingConvos !== null) {
-        const filteredExistingConvos = existingConvos.filter((convo) => convo.users.length === (req.body.usersTba.length + 1));
-        if (filteredExistingConvos.length > 0)
-          return res.status(500).json({message: "Found existing group conversations with same members",
-                                       conversations: filteredExistingConvos})
-      } 
-
-      // Verify all users are related to currentUser either through friends or in an active conversation
-      const currentUser = await User.findById(req.body.userId);
-      const allUserConvos = await Conversation.find({users: req.body.userId}, {users: 1})
-      const relatedUsers = new Set(allUserConvos.map((convo) => convo.users.map((id) => id.toString())).flat());
-      for (const userTba of req.body.usersTba) {
-        if (!currentUser.friends.includes(userTba) && !relatedUsers.has(userTba)) {
-          return res.status(500).json({message: "No relation with user found",
-                                       user: userTba});
+    // Check if conversation with same members already exists
+    const existingConvos = await Conversation.find({users: req.body.usersTba});
+    if (existingConvos !== null) {
+      if (req.body.usersTba.length == 2) {
+        const filteredExistingConvos = existingConvos.filter((convo) => convo.type === 'pair' && convo.active === true);
+        if (filteredExistingConvos.length > 0) {
+          return res.status(500).json({message: "Found existing direct conversations with same members",
+                                       conversations: filteredExistingConvos[0]})
         }
       }
+      else {
+        return res.status(500).json({message: "Found existing group conversations with same members",
+                                     conversations: existingConvos[0]})
+      }
+    } 
 
+    // Verify all users are related to currentUser either through friends or in an active conversation
+    const currentUser = await User.findById(req.body.userId);
+    const allUserConvos = await Conversation.find({users: req.body.userId, active: true}, {users: 1})
+    const relatedUsers = new Set(allUserConvos.map((convo) => convo.users.map((id) => id.toString())).flat());
+    for (const userTba of req.body.usersTba) {
+      if (!currentUser.friends.includes(userTba) && 
+          !relatedUsers.has(userTba) &&
+          userTba !== req.body.userId) {
+        return res.status(500).json({message: "No relation with user found",
+                                     user: userTba});
+      }
+    }
+
+    // Create conversation, if pair and conversation is inactive instead 
+    // make it active
+    if (req.body.usersTba.length > 2) {
       const newConversation = await new Conversation({
-        users: [req.body.userId, ...req.body.usersTba],
+        users: [req.body.usersTba],
         type: 'group',
         parentType: 'group'
       })
-
+  
       const conversation = await newConversation.save();
       return res.status(200).json(conversation);
+    } else if (existingConvos === null) {
+      const newConversation = await new Conversation({
+        users: [req.body.usersTba],
+        type: 'pair',
+        parentType: 'group'
+      })
+  
+      const conversation = await newConversation.save();
+      return res.status(200).json(conversation);
+    } else {
+      // Change pre-existing inactive conversation to active
+      const conversation = await Conversation.findOneAndUpdate({users: req.body.usersTba, 
+                                                                type: 'pair', 
+                                                                active: false}, 
+                                                               {active: true});
+      return res.status(200).json(conversation);
     }
+    
   } catch (err) {
     return res.status(500).json({message: err.message});
   }
