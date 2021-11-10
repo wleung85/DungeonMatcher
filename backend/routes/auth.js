10dc8ae1f4cf356a5e511c8ed63a366b5bc8edef
Winston Leung <wleung85@uwo.ca>
2021-11-09 22:49:33 -0500

    Started infrastructure with refresh tokens, before implementation into database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { authenticateToken } = require('../middleware/authenticate');
 
 // REGISTER
 router.post('/register', async (req, res) => {
@@ -25,6 +26,20 @@ router.post('/register', async (req, res) => {
   }
 });
 
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    { userId: user._id, isAdmin: user.isAdmin }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m"});
+}
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { userId: user._id, isAdmin: user.isAdmin }, 
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "30d"});
+}
+
 // LOGIN
 router.post('/login', async (req, res) => {
   try {
@@ -36,13 +51,39 @@ router.post('/login', async (req, res) => {
     if (!validPassword)
       return res.status(400).json({message: "Wrong password"});
 
-    const accessToken = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.ACCESS_TOKEN_SECRET);
-    res.status(200).json({accessToken: accessToken});
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+    // TODO: push refresh token to database
+    res.status(200).json({accessToken: accessToken, refreshToken: refreshToken});
   } catch (err) {
     console.log(err);
     res.status(500).json({message: err.message});
   }
   
 })
+
+// Refresh token
+router.post('/token', async (req, res) => {
+  // Take refresh token from user
+  const refreshToken = req.body.token;
+
+  // Send error if there is no token or is invalid
+  if (!refreshToken) return res.sendStatus(401);
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    // TODO: if refresh token is not in database, return error
+
+    // If everything is okay, create new access token
+    const newAccessToken = generateAccessToken(user);
+    res.status(200).json({accessToken: newAccessToken});
+  })
+});
+
+// LOGOUT
+router.post('/logout', authenticateToken, (req, res) => {
+  // TODO: find and delete refresh token in database
+  res.status(200).json("Successfully logged out");
+});
 
 module.exports = router;
