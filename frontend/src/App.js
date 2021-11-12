ebf46b4fa9defac5fb823146062d828deb2b5373
Winston Leung <wleung85@uwo.ca>
2021-11-11 22:17:10 -0500

    Added basic login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/MainComponent';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
